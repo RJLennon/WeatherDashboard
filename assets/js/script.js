19a5ca3550dc05878b6a0f88c24f2e64f357ecf1
RJLennon <robjohnlennon@gmail.com>
2024-03-17 23:45:00 -0400

    added weather api data in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,32 @@ document.addEventListener('DOMContentLoaded', function() {
         event.preventDefault();
         const cityValue = document.getElementById('cityInput').value;
         console.log('City:', cityValue);
-    })
-})
+
+        //Construct the API URL using the cityValue
+        const apiKey = "b9021926c89843416e54f049e9cb56a9"
+        const cityApiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityValue}&limit=1&appid=${apiKey}`;
+
+        //Fetch the data from the API
+        fetch(cityApiURL)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                console.log('Data:', data);
+                const latitude = data[0].lat;
+                const longitude = data[0].lon;
+                console.log('Latitude:', latitude);
+                console.log('Longitude:', longitude);
+
+                const weatherApiURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+                fetch(weatherApiURL)
+                    .then(function(response) {
+                        return response.json();
+                    })
+                    .then(function(data) {
+                        console.log('Weather Data:', data);
+                    });
+            });
+    });
+});
 
